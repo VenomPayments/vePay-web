d34e1237f6a8a3b767da5bfcf78c2eef659f2f07
Nikita Tkach @F0RES <i@ntkach.ru>
2023-06-10 12:52:44 +0700

    fix

--- src/modules/Dashboard/page/DashboardPage.tsx
+++ src/modules/Dashboard/page/DashboardPage.tsx
@@ -17,16 +17,9 @@ import { useRpcProvider } from '@broxus/js-core'
 
 
 export default function DashboardPage(): JSX.Element {
-    const intl = useIntl()
-    const wallet = useTvmWallet()
-    const provider = useRpcProvider("venom")
 
-    // const pay = new VePay("0:d5202656dea2ce2518fb6dfb4a81d4ed6e8bb8898c078309b33faeca7fdb79a6", provider)
     return (
         <>
-            {/* <p onClick={() => pay.pay('1', '0.1')}>
-                pay
-            </p> */}
             <Dashboard />
         </>
     )
